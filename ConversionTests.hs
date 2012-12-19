4d6cd8f2cc1d0255d7dab05a8626e75dc5d9416f
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-12-19 14:27:32 -0500

    WIP

--- ConversionTests.hs
+++ ConversionTests.hs
@@ -5,16 +5,28 @@ import Turing
 import Conversion
 import Test.HUnit
 
+abc = "abcd"
+
+test_convSym :: Test
+test_convSym = convSym abc 'a' ~?=
+               (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $
+                (var "'a'"))
 
 test_convTape :: Test
 test_convTape =
-  convTape "abcd" "acd" ~?=
+  convTape abc "acd" ~?=
   (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $ (var "'a'") <->
    (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $ (var "'c'") <->
-    (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $ (var "'d'") <->
-     (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $ (var "__")))))
+    (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $
+     (var "'d'") <->
+     (lam "'a'" $ lam "'b'" $ lam "'c'" $ lam "'d'" $ lam "__" $
+      (var "__")))))
 
-
+test_cons :: Test
+test_cons = TestList [
+  ((cons abc <-> (convSym abc 'a') <-> (convTape abc "bc"))) ~?=
+  ((convTape abc "abc"))
+  ]
 
 {-
 prop_conv :: TM -> String -> Bool
@@ -24,6 +36,8 @@ prop_conv m t = nf $ (execTerm m) <-> (initTerm m) tapeTerm $ listFromTape $ run
 main :: IO ()
 main = do
   _ <- runTestTT $ TestList [
-    test_convTape
+    test_convTape,
+    test_convSym,    
+    test_cons
     ]
   return ()
